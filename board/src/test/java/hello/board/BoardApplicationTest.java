59c0566292d857d6076228d354d6aba3f2d0bdbf
윤찬 <vsvx13@naver.com>
2025-06-30 21:14:38 +0900

    [board] 게시판 서비스 초기화

---
+++ board/src/test/java/hello/board/BoardApplicationTest.java
@@ -0,0 +1,14 @@
+package hello.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BoardApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
