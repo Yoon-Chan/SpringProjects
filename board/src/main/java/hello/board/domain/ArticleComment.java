cb9eb0cd2b4522e71afaa5f5567a9abf7c2f9456
윤찬 <vsvx13@naver.com>
2025-06-30 22:03:22 +0900

    [board][feat] Article & ArticleCoomment 클래스 설정

---
+++ board/src/main/java/hello/board/domain/ArticleComment.java
@@ -0,0 +1,14 @@
+package hello.board.domain;
+
+import java.time.LocalDateTime;
+
+public class ArticleComment {
+    private Long id;
+    private Long articleId;
+    private String content;
+
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private LocalDateTime modifiedAt;
+    private String modifiedBy;
+}
