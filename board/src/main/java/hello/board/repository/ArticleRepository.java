cb942c52f4ab1cf069f3486ebf669fc50130ae61
윤찬 <vsvx13@naver.com>
2025-07-01 18:10:59 +0900

    [board][feat] mysql 재설정 및 data rest, data rest hal 라이브러리 적용

--- board/src/main/java/hello/board/repository/ArticleRepository.java
+++ board/src/main/java/hello/board/repository/ArticleRepository.java
@@ -2,8 +2,10 @@ package hello.board.repository;
 
 import hello.board.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 //인터페이스로 구현하고 JpaRepository는 extends로 상속받기
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 
 }
