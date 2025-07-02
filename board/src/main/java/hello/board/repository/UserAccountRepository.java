d8ec1ad5c416bb1cc2153729b4dbe0343e39c014
윤찬 <vsvx13@naver.com>
2025-07-02 13:36:23 +0900

    [board][feat] UserAccout 데이터 추가

---
+++ board/src/main/java/hello/board/repository/UserAccountRepository.java
@@ -0,0 +1,7 @@
+package hello.board.repository;
+
+import hello.board.domain.UserAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
+}
