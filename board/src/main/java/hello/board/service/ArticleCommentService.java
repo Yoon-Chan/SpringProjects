72b0b44807da5c5f9fcc03fe8f4d57f7d0d8d480
윤찬 <vsvx13@naver.com>
2025-07-03 12:17:51 +0900

    [board][feat] 게시판 서비스 뷰에 기능 구현

--- board/src/main/java/hello/board/service/ArticleCommentService.java
+++ board/src/main/java/hello/board/service/ArticleCommentService.java
@@ -1,8 +1,12 @@
 package hello.board.service;
 
+import hello.board.domain.ArticleComment;
 import hello.board.dto.ArticleCommentDto;
 import hello.board.repository.ArticleCommentRepository;
+import hello.board.repository.ArticleRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,22 +14,42 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @Transactional
+@Slf4j
 @Service
 public class ArticleCommentService {
 
+    private final ArticleRepository articleRepository;
     private final ArticleCommentRepository articleCommentRepository;
+
 
     @Transactional(readOnly = true)
     public List<ArticleCommentDto> searchArticleComments(Long articleId) {
-        return List.of();
+        return articleCommentRepository.findByArticle_Id(articleId)
+                .stream()
+                .map(ArticleCommentDto::from)
+                .toList();
     }
 
     public void saveArticleComment(ArticleCommentDto dto) {
+        try {
+            articleCommentRepository.save(dto.toEntity(articleRepository.getReferenceById(dto.articleId())));
+        } catch (EntityNotFoundException e) {
+            log.warn("댓글 저장 실패. 댓글의 게시글을 찾을 수 없습니다 - dto: {}", dto);
+        }
     }
 
     public void updateArticleComment(ArticleCommentDto dto) {
+        try {
+            ArticleComment articleComment = articleCommentRepository.getReferenceById(dto.id());
+            if (dto.content() != null) {
+                articleComment.setContent(dto.content());
+            }
+        } catch (EntityNotFoundException e) {
+            log.warn("댓글 업데이트 실패. 댓글을 찾을 수 없습니다 - dto: {}", dto);
+        }
     }
 
     public void deleteArticleComment(Long articleCommentId) {
+        articleCommentRepository.deleteById(articleCommentId);
     }
 }
