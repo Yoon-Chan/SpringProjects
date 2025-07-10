360df73a2ba0b917d767a21c7e96be92f60dbcd4
윤찬 <vsvx13@naver.com>
2025-07-10 16:36:54 +0900

    [PT][init] 프로젝트 초기화

---
+++ pt_batch_project/src/main/java/hello/pt_batch_project/PtBatchProjectApplication.java
@@ -0,0 +1,13 @@
+package hello.pt_batch_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtBatchProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PtBatchProjectApplication.class, args);
+    }
+
+}
---
+++ pt_batch_project/src/test/java/hello/pt_batch_project/PtBatchProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.pt_batch_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtBatchProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
